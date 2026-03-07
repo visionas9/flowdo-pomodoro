0d02a453fb043dfa25b9921ce7e98269ec735394
Alperen Sirli <alperensirli99@hotmail.com>
2026-03-07 22:13:44 +0100

    style: buttons hover effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className=" text-mint-cream font-main">
+    <div className="flex flex-col items-center text-mint-cream font-main">
       <header className="flex items-center w-full p-4 md:max-w-2xl mx-auto">
         {/* add toggle theme button later */}
         <div className="flex flex-col flex-1 items-center justify-center">
@@ -8,7 +8,10 @@ export default function Home() {
           <p className="text-sm text-lighter">productivity app</p>
         </div>
         <div>
-          <button className="bg-darkdiv py-2 px-3 rounded-xl cursor-pointer font-bold">
+          <button
+            className="bg-darkdiv py-2 px-3 rounded-xl cursor-pointer font-bold
+          hover:bg-darkb-hover transition-colors duration-200"
+          >
             Log In
           </button>
         </div>
@@ -20,14 +23,32 @@ export default function Home() {
         className="mt-15 mx-auto w-fit flex flex-col 
       items-center bg-darkdiv py-3 px-5 rounded-xl md:"
       >
-        <h2>Set your timer:</h2>
-        <div>
+        <div className="flex items-center justify-between gap-9 md:">
+          <button
+            className="bg-darkb py-2 px-3 rounded-xl cursor-pointer
+          hover:bg-darkb-hover transition-colors duration-200 md:"
+          >
+            set work time
+          </button>
+          <button
+            className="bg-darkb py-2 px-3 rounded-xl cursor-pointer
+          hover:bg-darkb-hover transition-colors duration-200 md:"
+          >
+            set break time
+          </button>
+        </div>
+
+        <div className="flex items-center justify-center">
           {/*hardcoded for now */}
           <p className="font-numbers text-9xl">25:00</p>
         </div>
       </div>
       <div className="mt-6 text-3xl flex items-center justify-center md:">
-        <button className="py-2 px-5 rounded-xl cursor-pointer font-bold bg-start">
+        {/* Conditionally rendering button /  */}
+        <button
+          className="py-2 px-8 rounded-xl cursor-pointer font-bold bg-start
+        active:translate-y-1 active:shadow-none shadow-md transition-all duration-100"
+        >
           Start
         </button>
       </div>
