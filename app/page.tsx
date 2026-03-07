37344626dff49f86e963922abc5808069ca2d426
Alperen Sirli <alperensirli99@hotmail.com>
2026-03-07 21:53:32 +0100

    start button

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className=" text-mint-cream font-main">
       <header className="flex items-center w-full p-4 md:max-w-2xl mx-auto">
+        {/* add toggle theme button later */}
         <div className="flex flex-col flex-1 items-center justify-center">
           <h1 className="text-3xl font-extrabold">FlowDo</h1>
           <p className="text-sm text-lighter">productivity app</p>
@@ -24,6 +25,11 @@ export default function Home() {
           {/*hardcoded for now */}
           <p className="font-numbers text-9xl">25:00</p>
         </div>
+      </div>
+      <div className="mt-6 text-3xl flex items-center justify-center md:">
+        <button className="py-2 px-5 rounded-xl cursor-pointer font-bold bg-start">
+          Start
+        </button>
       </div>
     </div>
   );
