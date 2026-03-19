42b42a370ca2d492b312cbb904ac42cd836cf3a2
Alperen Sirli <alperensirli99@hotmail.com>
2026-03-19 15:40:41 +0100

    style: quotes align

--- app/components/Quotes.tsx
+++ app/components/Quotes.tsx
@@ -2,15 +2,15 @@ export default async function Quotes() {
   const data = await fetch("https://zenquotes.io/api/random");
   const posts = await data.json();
   return (
-    <ul>
+    <div>
       {posts.map((post: any) => (
         <div
           key={post.q}
-          className="mt-6 w-[90%] md:max-w-2xl mx-auto flex flex-col gap-3 bg-darkdiv py-4 px-5 rounded-xl"
+          className="text-center mt-10 w-[90%] md:max-w-2xl mx-auto flex flex-col gap-3 bg-darkdiv py-4 px-5 rounded-xl"
         >
           <p>"{post.q}"</p>
         </div>
       ))}
-    </ul>
+    </div>
   );
 }
